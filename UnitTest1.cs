eafeff9e6a85e7632d4dee5b04bec8c128ba0915
Студенты Черепановой А.О <cherao@teach.local>
2022-12-05 15:06:32 +0300

    Я БОГ ТЕСТИРОВАНИЯ!

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,10 +1,9 @@
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium;
-using OpenQA.Selenium.Support.UI;
+
 
 namespace SeleniumTests
 {
-    [TestFixture()]
     public class Test
     {
         private IWebDriver driver;
@@ -13,27 +12,27 @@ namespace SeleniumTests
         {
             driver = new ChromeDriver();
         }
-        [Test()]
+        [Test]
         public void TestCase1()
         {
             driver.Navigate().GoToUrl("https://shahty.ru/");
             driver.FindElement(By.ClassName("menu_top_block")).Click(); //переход "Новости"
             Assert.IsTrue(driver.Url.Contains("https://shahty.ru/news/"), "Курва1");
         }
-        [Test()]
+        [Test]
         public void TestCase2()
         {
             driver.Navigate().GoToUrl("https://shahty.ru/");
             driver.FindElement(By.CssSelector("[href*='/news/news_30591.html']")).Click(); //ткнуть картинку
             Assert.IsTrue(driver.Url.Contains("https://shahty.ru/news/news_30591.html"), "Курва2");
         }
-        [Test()]
+        [Test]
         public void TestCase3()
         {
             driver.Navigate().GoToUrl("https://shahty.ru/");
             driver.FindElement(By.ClassName("ya-site-form__input-text")).SendKeys("гей");
             driver.FindElement(By.ClassName("ya-site-form__submit")).Click();
-            driver.FindElement(By.ClassName("b-head__found"));
+            Assert.IsTrue(driver.FindElement(By.ClassName("b-head_type_search-simple")).FindElement(By.ClassName("b-head__found")).Text.Contains("нашёл"));
         }
     }
 }
